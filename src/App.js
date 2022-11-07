f4ed0a369ae523c40b3414a6417ae40503d20979
seumbae <ohcm0013@gmail.com>
2022-11-07 21:29:57 +0900

    디렉토리 구조 확립

--- src/App.js
+++ src/App.js
@@ -1,14 +1,7 @@
-import { BrowserRouter as Router, Routes, Route, Link} from "react-router-dom";
-import Login from "./pages/signin";
-import Signup from "./pages/signup";
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import Dev from "./pages/dev/dev";
-import Sec from "./pages/sec/sec";
-import Ops from "./pages/ops/ops";
-import Admin from "./pages/admin/admin";
 import { useState } from "react";
-import Logined from "./components/Logined";
 import DevHome from "./components/DevHome";
-import DevResult from "./components/DevResult";
 import DevIndex from "./components/DevIndex";
 
 function App() {
@@ -22,12 +15,6 @@ function App() {
           <Route path="dashboard" element={<DevHome/>}/>
         </Route>
       </Route>
-      {/* <Route path="/" element={<Login setLogin={setLogin} setName={setName}/>}></Route> */}
-      {/* <Route path="/signup" element={<Signup />}></Route> */}
-      {/* privte Routes */}
-      {/* <Route path="/sec" element={<Sec />}></Route> */}
-      {/* <Route path="/ops" element={<Ops />}></Route>  */}
-      {/* <Route path="/admin" element={<Admin />}></Route> */}
     </Routes>
     </>
   );
