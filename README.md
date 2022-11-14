4fdae5493fc651b0ed0cb2ddd2363e94eb7a62ed
nebulayoon4700 <nebulayoon4700@gmail.com>
2022-11-14 13:10:15 +0000

    fix: .env가 정상적으로 동작하지 않은 경우 고침
    readme.md에 사용법을 따로 작성했기 때문에 확인 후 사용

--- README.md
+++ README.md
@@ -1,7 +1,12 @@
 # 사용법
 ## .env
+무조건 REACT_APP_으로 시작해야함
 ```
-API_SERVER_BASEURL= -> axios에서 사용할 API 서버 경로
+REACT_APP_API_SERVER_BASEURL= -> axios에서 사용할 API 서버 경로
+```
+### 예시
+```
+REACT_APP_API_SERVER_BASEURL="http://IP:PORT/api/v1"
 ```
 
 ## Error: ENOSPC: System limit for number of file watchers reached
--- src/services/axios.jsx
+++ src/services/axios.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: process.env.API_SERVER_BASEURL,
+  baseURL: process.env.REACT_APP_API_SERVER_BASEURL,
   headers: {
     "Content-Type": "application/json",
   },
@@ -9,7 +9,6 @@ const API = axios.create({
 
 /* Login */
 export const tryLogin = async (data) => await API.post('/user/login', data);
-
 
 /* Security Result */
 export const getSecurityList = async () => await API.get('/security_result/securityResultList');
