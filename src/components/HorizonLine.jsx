2fc47fe8a91bd771e3f3f4b220624a972d1d51e7
seumbae <ohcm0013@gmail.com>
2022-11-15 02:45:22 +0900

    Create HorizonLine Component

---
+++ src/components/HorizonLine.jsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const Line = styled.div`
+  width: 100%;
+  border-bottom: 1px solid #D4D4D4;
+`
+
+const HorizonLine = () => {
+  return <Line />
+}
+export default HorizonLine;
