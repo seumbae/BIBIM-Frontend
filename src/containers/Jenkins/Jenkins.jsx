62494c40c2674458a86bf3bd2308c87804e0d997
seumbae <ohcm0013@gmail.com>
2022-11-18 06:35:51 +0900

    update: add loading, event

--- src/containers/Jenkins/Jenkins.jsx
+++ src/containers/Jenkins/Jenkins.jsx
@@ -2,6 +2,9 @@ import { useState } from "react";
 import styled from "styled-components";
 import Button from "./Button";
 import List from "./JenkinsList";
+import Modal from '@mui/material/Modal';
+import ListSkeleton from "../../components/ListSkeleton";
+import JenkinsCreate from "./JenkinsCreate";
 
 const BodyWrapper = styled.div`
 	margin-top: 3rem;
@@ -31,33 +34,62 @@ const JenkinsWrapper = styled.div`
 `
 
 const Jenkins = () => {
+	const [loading, setLoading] = useState(false);
+	const [create, setCreate] = useState(false);
+	const [created, setCreated] = useState(false);
+	const [checkList, setChecked] = useState({});
+	const [jenkinsList, setJenkinsList] = useState(['Pre-commit', 'Code-Ql', 'Zap', 'Dependabot']);
+	
+	const onHandleCreate = () => setCreate((prev) => !prev);
+	const onHandleClose = () => setCreate((prev) => !prev);
 	// 현재 Jenkins List 가져오는 API 미완성
-	const onHandleCreate = () => {
-		console.log("create");
-		// return modal
-	};
+
+	const onHandleCheck = (event) => {
+    const {value, checked} = event.target;
+    if(checked){
+      setChecked((prev) => ({...prev, [value]: checked}));
+    }
+    else{
+      const temp = {...checkList};
+      delete temp[value];
+      setChecked(temp);
+    }
+  }
+
 	const onHandleModify = () => {
 		console.log("modify");
 	};
 	const onHandleDelete = () => {
 		console.log("delete");
+		// Object.keys(checkList).forEach((key) => {
+    //   deletePipeline({pipeline_name: key}).then((res) => {
+    //     alert(res.data.msg);
+    //     setLoading(false);
+    //     window.location.reload();
+    //   })})
 	};
-	const [checked, setChecked] = useState(false);
-  const [jenkinsList, setJenkinsList] = useState(['Pre-commit', 'Code-Ql', 'Zap', 'Dependabot']);
+  
 	return (
 		<BodyWrapper>
 			<Buttons>
 				<Button onHandleCreate={onHandleCreate} />
-				{checked ? (
+				{Object.keys(checkList).length > 0 ? (
 					<ModifyDelete>
 						<Button name="수정" onHandleModify={onHandleModify} />
-						<Button name="삭제" onHandleDelete={onHandleDelete} />
+						<Button name="삭제" onHandleDelete={onHandleDelete}  />
 					</ModifyDelete>
 				) : null}
 			</Buttons>
       <JenkinsWrapper>
-        <List setChecked={setChecked} name="deploy_jenkinsfile_1.0.2" jenkins={jenkinsList} owner="nebulayoon" created="2022-10-12" updated="2022-10-12"/>
+			{ 
+			loading ? 
+        <ListSkeleton /> :
+				<List setChecked={setChecked} onHandleCheck={onHandleCheck} name="deploy_jenkinsfile_1.0.2" jenkins={jenkinsList} owner="nebulayoon" created="2022-10-12" updated="2022-10-12"/>
+      }
       </JenkinsWrapper>
+			<Modal open={create} onClose={onHandleClose} disableAutoFocus={true}>
+        <JenkinsCreate setCreate={setCreate} setCreated={setCreated}/>
+      </Modal>
 		</BodyWrapper>
 	);
 };
