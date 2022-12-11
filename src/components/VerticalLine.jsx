ce256e960d76bd8e72611ef027baa147758c37d7
seumbae <ohcm0013@gmail.com>
2022-12-12 03:16:45 +0900

    Add: create VerticalLine Component

---
+++ src/components/VerticalLine.jsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const Line = styled.div`
+  height: 100%;
+  border-left: 1px solid #D4D4D4;
+`
+
+const VerticalLine = () => {
+  return <Line />
+}
+export default VerticalLine;
