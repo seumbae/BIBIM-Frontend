33d133e70d0a4b7dd0208a15a832fe1b7c64192f
seumbae <ohcm0013@gmail.com>
2022-12-14 05:08:38 +0900

    update: add condition

--- src/components/detail-issue/AllIssueTable.jsx
+++ src/components/detail-issue/AllIssueTable.jsx
@@ -1,20 +1,20 @@
-import styled from "styled-components";
 import DetailIssueComponent from "./DetailIssue";
 
 const AllIssueTableList = ({ dataList }) => {
-  let { data } = dataList;
-  return(
-    <>
-      {
-        Array.isArray(data) ?
-          data.map((row, idx) => (
-            <DetailIssueComponent key={dataList["_id"]["$oid"] + "/" + idx.toString()} data={row} />
-          ))
-        :
-          null
-      }
-    </>
-  );
+	const { data } = dataList;
+	return (
+		<>
+			{data.length > 0
+				? data.map((item) => (
+						<DetailIssueComponent
+							key={dataList._id.$oid}
+							data={item}
+							pipelineName={dataList.pipelineName}
+						/>
+				  ))
+				: null}
+		</>
+	);
 };
 
 export default AllIssueTableList;
