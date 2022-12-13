99aa7f7e867031745fc9861b4a45b30212966c01
seumbae <ohcm0013@gmail.com>
2022-12-14 05:02:58 +0900

    update: add defualt route and project Detail Configuration

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
+import BuildContextProvider from "./store/BuildContext";
 import LoginPage from "./containers/Login/Login";
 import MainLayout from "./containers/common/MainLayout";
 import DetailLayout from "./containers/common/DetailLayout";
@@ -9,15 +10,15 @@ import Profile from "./containers/Profile/Profile"
 import DetailStatusPage from "./containers/ProjectDetail/Status/Status";
 import AllDetailIssueContainer from "./containers/detail-issue/AllDetailIssue";
 import ConfigurationContainer from "./containers/user/Configuration";
-// import Header from "./components/common/Header";
-// import MainNav from "./components/common/MainNav";
 import Jenkins from "./containers/Jenkins/Jenkins";
+import ProjectConfiguration from "./containers/ProjectDetail/Configuration/Configuration";
 
 /* 로그인 여부 확인해야함 */
 function App() {
   return (
     <Routes>
-      <Route path="login" element={<LoginPage />}></Route>
+      <Route path="" element={<Navigate to="/login" />} />
+      <Route path="/login" element={<LoginPage />}></Route>
       <Route element={<MainLayout />}>
         <Route path="/dev" element={<Dashboard />} /> 
         <Route path="/dev/profile" element={<Profile />} />
@@ -30,7 +31,7 @@ function App() {
         <Route path="/dev/:projectTitle/profile" element={<Issues />} />
         <Route path="/dev/:projectTitle/issues" element={<Issues />} />
         <Route path="/dev/:projectTitle/history" element={<Issues />} />
-        <Route path="/dev/:projectTitle/configuration" element={<Issues />} />
+        <Route path="/dev/:projectTitle/configuration" element={<ProjectConfiguration />} />
       </Route>
       <Route path="/configuration" element={<ConfigurationContainer />} />
     </Routes>
