25bb619a20134874ef6418c7593feef25f269d39
seumbae <ohcm0013@gmail.com>
2022-12-14 05:09:23 +0900

    chore: refactoring

--- src/containers/common/DetailNav.jsx
+++ src/containers/common/DetailNav.jsx
@@ -1,16 +1,19 @@
 import styled from "styled-components";
 import { useLocation, Link, useParams, useNavigate } from "react-router-dom";
-import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
-
+import { BuildContext } from "../../store/BuildContext";
+import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
 import BuildStatus from "../../components/BuildStatus";
+import { useContext, useLayoutEffect } from "react";
+import { useState } from "react";
+
 const Nav = styled.div`
-  display: flex;
-  flex-direction: column;
+	display: flex;
+	flex-direction: column;
 	width: 13rem;
-  margin-left: 2.5rem;
-  min-width: 11rem;
+	margin-left: 2.5rem;
+	min-width: 11rem;
 	heigth: 100vh;
-`
+`;
 const NavWarpper = styled.div`
 	background-color: #ffffff;
 	display: flex;
@@ -19,62 +22,96 @@ const NavWarpper = styled.div`
 `;
 const ContentWrapper = styled.div`
 	background-color: ${(props) => (props.active ? "#EEEEF2" : "#FFFFFF")};
-  height: 2.5rem;
-  border-radius: 6.4px;
-  display: flex;
-  align-items: center;
+	height: 2.5rem;
+	border-radius: 6.4px;
+	display: flex;
+	align-items: center;
 `;
 const IconWrapper = styled.div`
-  display: flex;
-  height: 3rem;
-  align-items: center;
-`
+	display: flex;
+	height: 3rem;
+	align-items: center;
+`;
 const Icon = styled(ArrowBackIosIcon)`
-  font-size: 1rem Important!;
-  cursor: pointer;
-  padding-left: 1rem;
-`
+	font-size: 1rem Important!;
+	cursor: pointer;
+	padding-left: 1rem;
+`;
 const Content = styled(Link)`
-  text-decoration: none;
-  color: inherit;
-  font-size: 1.2rem;
-  cursor: pointer;
-  padding-left: 1rem;
-`
+	text-decoration: none;
+	color: inherit;
+	font-size: 1.2rem;
+	cursor: pointer;
+	padding-left: 1rem;
+`;
 function isActive(path, location) {
 	//path가 현재 url과 같으면 true를 반환
 	return path === location;
 }
 
 function MainSideNav() {
-  const location = useLocation();
-  const params = useParams();
-  const navigate = useNavigate();
+	const buildContext = useContext(BuildContext);
+	const location = useLocation();
+	const params = useParams();
+	const navigate = useNavigate();
+	const [projectList, setProjectList] = useState([]);
+
+	useLayoutEffect(() => {
+		setProjectList(buildContext.pipeline);
+	},[buildContext.pipeline])
+
 	return (
-    <Nav>
-      <IconWrapper>
-        <Icon onClick={() => navigate(-1)}/>
-      </IconWrapper>
-		  <NavWarpper>
-		  	<ContentWrapper active={isActive(`/dev/${params.projectTitle}/status`,location.pathname)}>
-		  		<Content to={`/dev/${params.projectTitle}/status`}>Status</Content>
-		  	</ContentWrapper>
-		  	<ContentWrapper active={isActive(`/dev/${params.projectTitle}/profile`, location.pathname)}>
-		  		<Content to={`/dev/${params.projectTitle}/profile`}>Profile</Content>
-		  	</ContentWrapper>
-		  	<ContentWrapper active={isActive(`/dev/${params.projectTitle}/issues`, location.pathname)}>
-		  		<Content to={`/dev/${params.projectTitle}/issues`}>Issues</Content>
-		  	</ContentWrapper>
-		  	<ContentWrapper active={isActive(`/dev/${params.projectTitle}/history`, location.pathname)}>
-		  		<Content to={`/dev/${params.projectTitle}/history`}>History</Content>
-		  	</ContentWrapper>
-		  	<ContentWrapper active={isActive(`/dev/${params.projectTitle}/configuration`, location.pathname)}>
-		  		<Content to={`/dev/${params.projectTitle}/configuration`}>Configuration</Content>
-		  	</ContentWrapper>
+		<Nav>
+			<IconWrapper>
+				<Icon onClick={() => navigate(-1)} />
+			</IconWrapper>
+			<NavWarpper>
+				<ContentWrapper
+					active={isActive(
+						`/dev/${params.projectTitle}/status`,
+						location.pathname
+					)}
+				>
+					<Content to={`/dev/${params.projectTitle}/status`}>Status</Content>
+				</ContentWrapper>
+				<ContentWrapper
+					active={isActive(
+						`/dev/${params.projectTitle}/profile`,
+						location.pathname
+					)}
+				>
+					<Content to={`/dev/${params.projectTitle}/profile`}>Profile</Content>
+				</ContentWrapper>
+				<ContentWrapper
+					active={isActive(
+						`/dev/${params.projectTitle}/issues`,
+						location.pathname
+					)}
+				>
+					<Content to={`/dev/${params.projectTitle}/issues`}>Issues</Content>
+				</ContentWrapper>
+				<ContentWrapper
+					active={isActive(
+						`/dev/${params.projectTitle}/history`,
+						location.pathname
+					)}
+				>
+					<Content to={`/dev/${params.projectTitle}/history`}>History</Content>
+				</ContentWrapper>
+				<ContentWrapper
+					active={isActive(
+						`/dev/${params.projectTitle}/configuration`,
+						location.pathname
+					)}
+				>
+					<Content to={`/dev/${params.projectTitle}/configuration`}>
+						Configuration
+					</Content>
+				</ContentWrapper>
 				{/* TODO: Context 사용하여 build 중일 때 build하고 있는 것 보여주기 */}
 				<BuildStatus />
-		  </NavWarpper>
-    </Nav>
+			</NavWarpper>
+		</Nav>
 	);
 }
 
