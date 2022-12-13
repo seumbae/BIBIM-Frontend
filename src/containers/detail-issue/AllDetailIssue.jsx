78a429da938ff116aed7de339f67933f1c931c34
seumbae <ohcm0013@gmail.com>
2022-12-14 05:10:33 +0900

    update: add filter

--- src/containers/detail-issue/AllDetailIssue.jsx
+++ src/containers/detail-issue/AllDetailIssue.jsx
@@ -1,31 +1,91 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect } from "react";
 import { getSecurityList } from "../../services/axios";
 import AllIssueTableList from "../../components/detail-issue/AllIssueTable";
 import styled from "styled-components";
+import Filter from "../../components/Filter";
 
 const ResultArea = styled.div`
-  display: block;
-  flex: 1;
+	margin-top: 3rem;
+	display: flex;
+	gap: 1rem;
+	flex: 1;
+	margin-right: 9%;
+`;
+
+const Lists = styled.div`
+	display: flex;
+	flex-direction: column;
+	flex: 1;
+	gap: 0.5rem;
+`;
+
+const None = styled.div`
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	flex: 1;
+	color: #4A4949;
+	font-size: 0.8rem;
 `;
 
 const AllDetailIssueContainer = () => {
-  const [issueData, setIssueData] = useState([]);
+	const [issueData, setIssueData] = useState([]);
 
-  useEffect(() => {
-    getSecurityList().then((res) => {
-      setIssueData(JSON.parse(res.data.result))
-    })
-  }, []);
-  
-  return (
-    <ResultArea>
-      {
-        issueData.map((oneProjectItem) => (
-          <AllIssueTableList key={oneProjectItem["_id"]["$oid"]} dataList={oneProjectItem} />
-        ))
-      }
-    </ResultArea>
-  );
+	const [data, setData] = useState({
+		Languages: ["Javascript", "Python"],
+		Stages: {
+			SIS: ["Gitleaks", "GitGuardian"],
+			SAST: ["CodeQL", "Semgrep"],
+			DAST: ["OWASP ZAP"],
+			SCA: ["OWASP DependencyCheck"],
+		},
+		Tag: ["CWE", "OWASP"],
+		"Security Category": {
+			CWE: [
+				"CWE-787 - Out of bounds write",
+				"CWE-79 - Cross-Site Scription (XSS)",
+				"CWE-89 - SQL Injection",
+				"CWE-20 - Improper Input Validation",
+				"CWE-125 - Out of bounds read",
+				"CWE-78 - OS Command Injection",
+				"CWE-416 - Use After Free",
+				"CWE-22 - Path Traversal",
+				"CWE-352 - Cross-Site Request Forgery (CSRF)",
+			],
+		},
+	});
+
+	const [checked, setChecked] = useState({
+		Languages: [],
+		SIS: [],
+		SAST: [],
+		DAST: [],
+		SCA: [],
+		Tag: [],
+	});
+	useEffect(() => {
+		getSecurityList().then((res) => {
+			setIssueData(JSON.parse(res.data.result));
+		});
+	}, []);
+
+	return (
+		<ResultArea>
+			<Filter data={data} checked={checked} setChecked={setChecked} />
+			{issueData.length > 0 ? (
+				<Lists>
+					{issueData.map((oneProjectItem) => (
+						<AllIssueTableList
+							key={oneProjectItem["_id"]["$oid"]}
+							dataList={oneProjectItem}
+						/>
+					))}
+				</Lists>
+			) : (
+				<None>검사결과가 존재하지 않아 Issue가 존재하지 않습니다.</None>
+			)}
+		</ResultArea>
+	);
 };
 
 export default AllDetailIssueContainer;
