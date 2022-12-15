310231b47020da8010be4dab4e4e3c836546069e
seumbae <ohcm0013@gmail.com>
2022-12-16 05:48:30 +0900

    update: add multiple APIs

--- src/services/axios.jsx
+++ src/services/axios.jsx
@@ -14,6 +14,8 @@ export const tryLogin = async (data) => await API.post('/user/login', data);
 export const getSecurityList = async () => await API.get('/security_result/bibimResultList');
 export const getProjectSecurityResult = async (id) => await API.get(`/projectTotalSecurityResult/${id}`);
 export const getStageIssueCount = async () => await API.get('/security_result/dashboardStageIssueCount');
+export const allProjectPrecisionCount = async () => await API.get('/security_result/allProjectPrecisionCount');
+export const projectPrecisionCount = async (pipelineName) => await API.get(`/security_result/projectPrecisionCount/${pipelineName}`);
 
 
 /* Get Security Check List */
@@ -24,7 +26,11 @@ export const getToolList = async () => await API.get('/tool/toolList');
 export const createPipeline = async (data) => await API.post('/pipeline/createPipeline', data);
 export const deletePipeline = async (data) => await API.post('/pipeline/deletePipeline', data);
 export const runPipeline = async (data) => await API.post('/pipeline/runPipeline', data);
-export const getPipeline = async () => await API.get('/pipeline/pipelineList');
+export const getPipelineList = async () => await API.get('/pipeline/pipelineList');
+export const getPipeline = async (data) => await API.post('/pipeline/getPipeline', data);
+export const getStream = async (data) => await API.post('/pipeline/getStream', data);
 
+/* JenkinsFile */
+export const getJenkinslist = async () => await API.get('/pipeline/getJenkinsfiles');
 
 export default API;
